ccd0226e30a9b3854940867006d48e389dcf6be9
lab_admin <none@none.none>
2021-01-25 10:56:50 +0000

    Probably added working training loop.

--- read_carla_stereo.py
+++ read_carla_stereo.py
@@ -9,9 +9,35 @@ import re
 import cv2
 import numpy as np
 import yaml
+from scipy.stats import binom
 from skimage import io
 from pathlib import Path
 from torch.utils.data import Dataset, DataLoader
+
+
+def preprocess_disparity(disp, maxdisp):
+    # we will use binomial dist to generate random vectors
+    result = np.zeros((maxdisp, disp.shape[0], disp.shape[1]), dtype=float)
+    disparities = list(range(maxdisp))
+    for i in range(disp.shape[0]):
+        for j in range(disp.shape[1]):
+            d = disp[i, j]
+            a = int(np.floor(d))
+            b = int(np.ceil(d))
+            y = (a-d)/(a-b)
+            x = 1-y
+            r = np.zeros(maxdisp, dtype=float)
+            r[a] = x
+            r[b] = y
+            result[:, i, j] = r
+            '''
+            p = disp[i, j]/maxdisp
+            rv = binom(maxdisp, p)
+            rv = rv.pmf(disparities)
+            result[i,j,:] = rv
+            '''
+    return result
+
 
 def preprocess_paths(l_list, r_list, d_list):
     '''
@@ -81,7 +107,8 @@ class CarlaDataset(Dataset):
         return np.array(K_arr)
     
     
-    def __init__(self, root_dir=Path.cwd(), target_height=128, target_width=256, original_height=1217, original_width=1937, fov=100):
+    def __init__(self, root_dir=Path.cwd(), max_disp=32, h=128, w=256, original_height=1217, original_width=1937, fov=100):
+        self.max_disp = max_disp
         # load stf
         left_im_dir = root_dir / "left"
         right_im_dir = root_dir / "right"
@@ -92,13 +119,13 @@ class CarlaDataset(Dataset):
         # match corresponding frames
         self.data_paths = preprocess_paths(left_paths, right_paths, depth_paths)
         #calculate size after scaling
-        self.dsize = (target_width, target_height)
+        self.dsize = (w, h)
         #load K-matrix
         self.K_mat = CarlaDataset._load_K("K.yml")
         #transform K-matrix
-        a = 0
-        b = 0/2 - 0.5
-        self.K_mat = np.array([[a, 0, b],[0, a, b],[0, 0, 1]]).dot(self.K_mat)
+        a = w/original_width
+        b = h/original_height
+        self.K_mat = np.diag([a,b,1.0]).dot(self.K_mat)
     
     
     def __len__(self):
@@ -117,7 +144,13 @@ class CarlaDataset(Dataset):
         imL = cv2.resize(imL, self.dsize)
         imR = cv2.resize(imR, self.dsize)
         imD = cv2.resize(imD, self.dsize)
-        return imL, imR, imD, dp[3]
+        # create disparity for training
+        imD = imD/255 # normalize
+        imD = 1000*imD # in_meters
+        B = 0.5
+        disp = B*self.K_mat[0, 0]/imD
+        disp = disp[:,:, np.newaxis]
+        return imL, imR, disp, dp[3]
     
 
 def test_1():
@@ -144,15 +177,15 @@ def test_2():
     abs_path = Path(r"E:\Patryk_Terechowicz\Carla_20_01")
     ds = CarlaDataset(root_dir=abs_path)
     print("Found %d valid data samples" % len(ds))
-    dsize = (256, 256)
+    dsize = (256, 128)
     for imL, imR, imD, frame_id in ds:
         imL = cv2.resize(imL, dsize)
         imR = cv2.resize(imR, dsize)
         imD = cv2.resize(imD, dsize)
-        im = np.zeros((256, 3*256, 4), dtype=np.uint8)
+        im = np.zeros((128, 3*256, 3), dtype=np.uint8)
         im[:,:256,:] = imL
         im[:, 256:2*256,:] = imR
-        im[:, 2*256:,:] = np.dstack([imD, imD, imD, imD])
+        im[:, 2*256:,:] = np.dstack([imD, imD, imD])
         cv2.imshow("Vid", im)
         cv2.waitKey(10)
     cv2.destroyAllWindows()
@@ -161,16 +194,26 @@ def test_2():
 def test_3():
     abs_path = Path(r"E:\Patryk_Terechowicz\Carla_20_01")
     ds = CarlaDataset(root_dir=abs_path)
-    imL, imR, imD, _ = ds[0]
+    imL, imR, disp, _ = ds[15]
     cv2.imshow("imL", imL)
     cv2.imshow("imR", imR)
-    cv2.imshow("imD", imD)
+    cv2.imshow("disparity", disp)
     cv2.waitKey(0)
     print(ds.dsize)
     print(ds.K_mat)
     return ds
 
 
+def test_preprocess_disp():
+    abs_path = Path(r"E:\Patryk_Terechowicz\Carla_20_01")
+    ds = CarlaDataset(root_dir=abs_path)
+    imL, imR, disp, _ = ds[15]
+    disp2 = preprocess_disparity(disp, ds.max_disp)
+    return disp2
+
+
 if __name__ == "__main__":
-    ds = test_3()
+    abs_path = Path(r"E:\Patryk_Terechowicz\Carla_20_01")
+    ds = CarlaDataset(root_dir=abs_path)
+    imL, imR, disp, _ = ds[15]
     cv2.destroyAllWindows()
--- train.py
+++ train.py
@@ -12,24 +12,26 @@ import torchvision
 import torchvision.transforms as transforms
 import progressbar
 import cv2
+import numpy as np
 from torch.optim import SGD, lr_scheduler
 from pathlib import Path
+from torch.autograd import Variable
 #our imports,
 import config
-from read_carla_stereo import CarlaDataset
+from read_carla_stereo import CarlaDataset, preprocess_disparity
 from gc_net import *
 
 
 
 # ===Load Train data===
 abs_path = Path(config.ds_paths['train'])
-ds_train = CarlaDataset(root_dir=abs_path)
+ds_train = CarlaDataset(root_dir=abs_path, max_disp=32, h=128, w=256)
 data_n0 = len(ds_train)
 print("==>Found %d valid data samples" % data_n0)
 # ===Create training model===
 h=ds_train.dsize[1]
 w=ds_train.dsize[0]
-maxdisp=1*32 
+maxdisp=ds_train.max_disp
 net = GcNet(h,w,maxdisp)
 net=torch.nn.DataParallel(net).cuda()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -44,20 +46,38 @@ lr_decay = config.train_conf['lr_decay']
 
 optimizer = SGD(net.parameters(), lr=lr, momentum=momentum)
 scheduler = lr_scheduler.ExponentialLR(optimizer, gamma=lr_decay)
+loss = torch.nn.L1Loss()
 # ===Define preprocessing===
-preprocessing = transforms.Compose([transforms.ToTensor()])
-
+tsfm = transforms.Compose([transforms.ToTensor(), transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+to_tensor = transforms.ToTensor()
 # ===Training loop===
+loss_mul_list = []
+for d in range(maxdisp):
+    loss_mul_temp = Variable(torch.FloatTensor(np.ones([1, 1, h, w]) * d)).cuda()
+    loss_mul_list.append(loss_mul_temp)
+loss_mul = torch.cat(loss_mul_list, 1)
 print("==>Starting training")
 for i in range(1, max_epochs+1):
     print("====>Epoch: [%d] \n" % i)
-    # Train data
+    net.train()
     for j in progressbar.progressbar(range(data_n0)):
-        imL, imR, imD, frame_id = ds_train[j]
+        net.zero_grad()
+        optimizer.zero_grad()
+        imL, imR, disp, frame_id = ds_train[j]
         #preprocess
-        imL = preprocessing(imL)
-        imR = preprocessing(imR)
-        result = net(torch.unsqueeze(imL, dim=0), torch.unsqueeze(imR, dim=0))
+        imL = tsfm(imL).cuda()
+        imR = tsfm(imR).cuda()
+        #disp = preprocess_disparity(disp, maxdisp)
         
+        disp = to_tensor(disp)
+        disp = disp.unsqueeze(0)
+        disp = disp.cuda()
+        x = net(torch.unsqueeze(imL, dim=0), torch.unsqueeze(imR, dim=0))
+        result=torch.sum(x.mul(loss_mul),1)
+        out = loss(result, disp)
+        out.backward()
+        optimizer.step()
+    scheduler.step()
+    
 
 cv2.destroyAllWindows()
